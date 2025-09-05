17855276b6eadf6e1488b02e54997a239caa6bff
vlbvvv <volobuievvv@gmail.com>
2025-09-05 04:03:13 +0200

    feedback optimization

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -98,21 +98,15 @@ function renderReviews(reviews) {
 function generateStars(rating) {
   const fullStars = Math.floor(rating);
   const totalStars = 5;
-  const svgStars = `<div class="star">
-      <svg class="star-empty" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false">
-        <path fill="#fff" d="M12 .587l3.668 7.431 8.2 1.192-5.934 5.787 
-        1.402 8.173L12 18.897l-7.336 3.86 
-        1.402-8.173L.132 9.21l8.2-1.192z"></path>
-      </svg>
-      <svg class="star-filled" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false">
-        <path fill="#764191" d="M12 .587l3.668 7.431 8.2 1.192-5.934 5.787 
+  let html = '';
+  for (let i = 1; i <= totalStars; i++) {
+    html += `<div class="star">
+      <svg class="${i <= fullStars ? 'star-filled' : 'star-empty'}" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false">
+        <path fill="${i <= fullStars ? '#764191' : '#fff'}" d="M12 .587l3.668 7.431 8.2 1.192-5.934 5.787 
         1.402 8.173L12 18.897l-7.336 3.86 
         1.402-8.173L.132 9.21l8.2-1.192z"></path>
       </svg>
     </div>`;
-  let html = '';
-  for (let i = 1; i <= totalStars; i++) {
-    html += svgStars;
   }
   return `<div class="star-container">${html}</div>`;
 }
